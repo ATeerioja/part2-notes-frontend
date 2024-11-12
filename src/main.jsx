1f892312742c8231b2b2c3dcb3efb1c764b1ab88
Anton Teerioja <anton.s.teerioja@utu.fi>
2024-11-12 20:24:53 +0200

    Added JSON-server

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
+import axios from 'axios'
+
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+    ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+  })
 
 const notes = [
   {
@@ -19,7 +27,3 @@ const notes = [
     important: true
   }
 ]
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
